8197777cd50124ce41693e9a30aa63e2bbe7b99e
rgsommer <63170479+rgsommer@users.noreply.github.com>
2025-11-05 20:02:29 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => res.send("Curriculate is alive!"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
