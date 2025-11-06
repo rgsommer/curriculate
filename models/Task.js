2a74ef335125581b8be857e2815a58439e794b22
rgsommer <63170479+rgsommer@users.noreply.github.com>
2025-11-05 21:55:20 -0500

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,11 @@ const taskSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     description: String,
-    points: { type: Number, default: 1 }
+    points: { type: Number, default: 1 },
+    // add fields from “the other day” here:
+    subject: String,
+    stationNumber: Number,
+    isLinear: { type: Boolean, default: false }
   },
   { timestamps: true }
 );
