f26ed3f79fcfd865a7970aae0429c0e1fddec3a3
rgsommer <63170479+rgsommer@users.noreply.github.com>
2025-11-08 12:27:18 -0500

    Update App.jsx

--- teacher-app/src/App.jsx
+++ teacher-app/src/App.jsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import LiveSession from "./pages/LiveSession.jsx";
-
-const SOCKET_URL = import.meta.env.VITE_API_URL;
-const socket = io(SOCKET_URL);
 
 export default function App() {
   return <LiveSession />;
