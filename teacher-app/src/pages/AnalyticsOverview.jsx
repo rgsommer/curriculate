8b46ccb91a8e0e6ab01c36c77b08f8bd6cff323f
Richard Sommer <rgsommer@me.com>
2025-11-22 17:17:02 -0500

    took reference to context out of AnalyticsOverview

--- teacher-app/src/pages/AnalyticsOverview.jsx
+++ teacher-app/src/pages/AnalyticsOverview.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../api/client";
-import { useAuth } from "../context/AuthContext";
 
 export default function AnalyticsOverview() {
   const { user } = useAuth();
