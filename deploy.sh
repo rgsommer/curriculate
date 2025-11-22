861ec4f331a19b05ad30bd435511271067d491d1
Richard Sommer <rgsommer@me.com>
2025-11-22 00:31:49 -0500

    index

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,17 @@
 git status
 read -p "Commit message: " msg
 
+echo "🔧 Adding changes..."
 git add .
+
+
+echo "🔧 Adding changes..."
+
+
 git commit -m "$msg"
+
+
 git push origin main
+
 
 echo "🔥 Deploy complete!"
