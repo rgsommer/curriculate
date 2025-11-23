bc9c91c264c6c0a1d27a5c382a3413c7976b43da
Richard Sommer <rgsommer@me.com>
2025-11-22 21:10:57 -0500

    student main update

--- student-app/src/main.jsx
+++ student-app/src/main.jsx
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./StudentApp.jsx";
 import './index.css';
 
+// student-app/src/main.jsx
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
