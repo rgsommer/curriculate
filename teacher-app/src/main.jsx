1ccea38433e79e66f3b60344800a7b43d7516b96
Richard Sommer <rgsommer@me.com>
2025-11-26 23:31:11 -0500

    TeacherApp

--- teacher-app/src/main.jsx
+++ teacher-app/src/main.jsx
@@ -1,7 +1,7 @@
 // teacher-app/src/main.jsx (or index.jsx)
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./TeacherApp";
 import { AuthProvider } from "./auth/useAuth";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
