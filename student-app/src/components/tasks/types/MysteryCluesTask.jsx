d181cfcb840c9c475e69e83aca8514ef4bc3270d
Richard Sommer <rgsommer@me.com>
2025-12-11 09:51:04 -0500

    victory screen path corrected

--- student-app/src/components/tasks/types/MysteryCluesTask.jsx
+++ student-app/src/components/tasks/types/MysteryCluesTask.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import VictoryScreen from "../VictoryScreen";
+import VictoryScreen from "../../VictoryScreen";
 
 const CARD_EMOJIS = [
   "Apple",
